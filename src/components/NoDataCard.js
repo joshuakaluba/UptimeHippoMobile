7241721c9fe7624e09ea6671c271c9318f9c2ad4
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 22:07:56 -0400

    Created NoDataCard

---
+++ src/components/NoDataCard.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Colors } from "../constants";
+import * as Icon from "@expo/vector-icons";
+import PrimaryButton from "./PrimaryButton";
+import PropTypes from "prop-types";
+
+export default class NoDataCard extends Component {
+  static propTypes = {
+    icon: PropTypes.string.isRequired,
+    noDataOutput: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <View style={styles.card}>
+        <Icon.Entypo
+          name={this.props.icon}
+          size={35}
+          color={Colors.lightGrey}
+        />
+        <Text style={styles.noRecords}>{this.props.noDataOutput}</Text>
+        {this.props.noDataActionCall && this.props.noDataActionTitle && (
+          <PrimaryButton
+            onPress={() => {
+              this.props.noDataActionCall();
+            }}
+            title={this.props.noDataActionTitle}
+          />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 15,
+    paddingTop: 50,
+    paddingBottom: 50,
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 25,
+    paddingRight: 25,
+    borderColor: Colors.lightGrey,
+    borderWidth: 2,
+    borderRadius: 12
+  },
+  noRecords: {
+    fontSize: 14,
+    fontWeight: "800",
+    marginTop: 30,
+    marginBottom: 30,
+    textAlign: "center",
+    color: Colors.darkGrey
+  }
+});
