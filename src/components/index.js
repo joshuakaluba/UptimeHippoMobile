abcb9e12556a23758075f2eb922ecb34ededf08a
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-22 07:54:55 -0500

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import PrimaryButton from './PrimaryButton';
+import NoDataCard from './NoDataCard';
+import MonitorList from './MonitorList';
+import MonitorInputDialog from './MonitorInputDialog';
+import PrimaryFormDropDown from './PrimaryFormDropDown';
+import MonitorLogsList from './MonitorLogsList';
+import MonitorLogsListItem from './MonitorLogsListItem';
+import PrimaryFormInput from './PrimaryFormInput';
+import TabBarIcon from './TabBarIcon';
+import Dashboard from './Dashboard';
+import LoadingModal from './LoadingModal';
+import SuccessCard from './SuccessCard';
+
+export {
+    PrimaryButton,
+    NoDataCard,
+    MonitorList,
+    MonitorInputDialog,
+    PrimaryFormDropDown,
+    MonitorLogsList,
+    MonitorLogsListItem,
+    PrimaryFormInput,
+    TabBarIcon,
+    Dashboard,
+    LoadingModal,
+    SuccessCard
+}
