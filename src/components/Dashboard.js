8a61092d20eec147fde25d5e8d123b9ae0be1206
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-21 19:18:01 -0500

    Created initial dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,88 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View, Text, StyleSheet } from "react-native";
+import { Colors, StringDictionary } from "../constants";
+import PrimaryButton from "./PrimaryButton";
+
+export default class Dashboard extends React.Component {
+  static propTypes = {
+    monitors: PropTypes.array.isRequired,
+    triggerStatusCheck: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired
+  };
+
+  /**
+   * if "lastMonitorSuccess": true, then site is online else  site is offline
+   *
+   * @param status which status to check
+   */
+  _getNumberOfCurrentlySitesStatus = status => {
+    if (!!this.props.monitors == false && this.props.monitors.length < 0) {
+      return 0;
+    }
+    return this.props.monitors.filter(monitor => {
+      return monitor.lastMonitorSuccess === status;
+    }).length;
+  };
+
+  render() {
+    return (
+      <View>
+        <View style={styles.card}>
+          <Text style={styles.cardHeaderText}>
+            {StringDictionary.currentlyOffline}
+          </Text>
+          <Text style={[styles.cardBodyText, { color: Colors.danger }]}>
+            {this._getNumberOfCurrentlySitesStatus(false)}
+          </Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardHeaderText}>
+            {StringDictionary.currentlyOnline}
+          </Text>
+          <Text style={[styles.cardBodyText, { color: Colors.success }]}>
+            {this._getNumberOfCurrentlySitesStatus(true)}
+          </Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardHeaderText}>{StringDictionary.sites}</Text>
+          <Text style={styles.cardBodyText}>
+            {this.props.monitors ? this.props.monitors.length : "0"}
+          </Text>
+        </View>
+        <View style={styles.statusCheckContainer}>
+          <PrimaryButton
+            title={StringDictionary.triggerStatusCheck}
+            onPress={this.props.triggerStatusCheck}
+            disabled={this.props.loading}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: Colors.white,
+    paddingTop: 30,
+    paddingBottom: 30,
+    alignItems: "center",
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginTop: 5,
+    borderWidth: 0.5,
+    borderColor: Colors.lightGrey
+  },
+  statusCheckContainer: {
+    alignItems: "center"
+  },
+  cardHeaderText: {
+    color: Colors.darkGrey,
+    fontWeight: "bold"
+  },
+  cardBodyText: {
+    fontSize: 30,
+    fontWeight: "bold"
+  }
+});
