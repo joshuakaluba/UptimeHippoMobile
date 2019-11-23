793359ba3537952db80b8a1104ba47981f748a04
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-22 19:53:52 -0500

    Create Colors.js

---
+++ src/constants/Colors.js
@@ -0,0 +1,51 @@
+const darkGrey = "#363636";
+const black = "#000";
+const white = "#fff";
+const danger = "tomato";
+const blue = "#2196F3";
+const tintColor = "#fff";
+const iconDefault = "#000";
+const copper = "#ec9b3b";
+const bodyBackgroundColor = "#f2f1ef";
+const scrollViewBackgroundColor = white;
+const mainViewBackgroundColor = "white";
+const tabHeaderBackgroundColor = darkGrey;
+const tabBarBackgroundColor = "#f5fbff";
+const grey = "#2e3131";
+const defaultTextColor = "#2e3131";
+const success = "#3fc380";
+const lightGrey = "#bdc3c7";
+const primary = darkGrey;
+
+export default Colors = {
+  blue,
+  tintColor,
+  accent: copper,
+  black,
+  white,
+  danger,
+  darkGrey,
+  success,
+  lightGrey,
+  defaultTextColor,
+  bodyBackgroundColor,
+  mainViewBackgroundColor,
+  scrollViewBackgroundColor,
+  primary,
+  iconDefault,
+  headerTintColor: white,
+  tabIconDefault: grey,
+  tabInactiveTintColor: grey,
+  tabActiveTintColor: white,
+  tabBarBackgroundColor,
+  tabIconSelected: primary,
+  tabBackgroundColor: tabHeaderBackgroundColor,
+  headerBackgroundColor: tabHeaderBackgroundColor,
+  tabBar: "#fefefe",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
+  noticeBackground: tintColor,
+  noticeText: "#fff"
+};
