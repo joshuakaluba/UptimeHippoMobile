3343208ba5bb42fab08f518c2ada368a87e75434
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-23 21:32:31 -0500

    Create index.js

---
+++ src/dataaccesslayer/index.js
@@ -0,0 +1,9 @@
+import UserAccountRepository from './UserAccountRepository';
+import MonitorRepository from "./MonitorRepository";
+import MonitorLogsRepository from './MonitorLogsRepository';
+
+export {
+    UserAccountRepository,
+    MonitorRepository,
+    MonitorLogsRepository
+}
