fc4fbd6bf2f7d58f628c218f590f960bf91588e0
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-23 21:31:49 -0500

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+import Colors from './Colors';
+import Layout from './Layout';
+import StringDictionary from './StringDictionary';
+import DefaultStyles from './DefaultStyles';
+import MonitorTypes from './MonitorTypes';
+import MonitorInterval from './MonitorInterval';
+import DeviceEvents from "./DeviceEvents";
+import Uri from './Uri';
+import ApplicationDefaultSettings from './ApplicationDefaultSettings';
+
+export {
+    Colors,
+    Layout,
+    StringDictionary,
+    DefaultStyles,
+    MonitorTypes,
+    MonitorInterval,
+    DeviceEvents,
+    Uri,
+    ApplicationDefaultSettings
+}
