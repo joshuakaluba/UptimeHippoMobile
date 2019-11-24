c8079e71a6ca530d68b177b2c9d46ed422c9447a
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-23 21:32:03 -0500

    Create MonitorTypes.js

---
+++ src/constants/MonitorTypes.js
@@ -0,0 +1,18 @@
+export default MonitorTypes = [
+  {
+    id: 0,
+    value: "HTTP/s"
+  },
+  {
+    id: 1,
+    value: "Keyword"
+  },
+  {
+    id: 2,
+    value: "Ping"
+  },
+  // {
+  //   id: 3,
+  //   value: "Port"
+  // }
+];
