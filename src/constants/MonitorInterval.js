b635814b4b5d5de182c68e5a8cdb7f719fe241a0
Joshua Kaluba <joshkaluba@gmail.com>
2019-11-23 21:31:56 -0500

    Create MonitorInterval.js

---
+++ src/constants/MonitorInterval.js
@@ -0,0 +1,34 @@
+export default MonitorInterval = [
+  // {
+  //   id: 0,
+  //   value: "Every Minute"
+  // },
+  {
+    id: 1,
+    value: "Every 5 Minutes"
+  },
+  {
+    id: 2,
+    value: "Every 10 Minutes"
+  },
+  {
+    id: 3,
+    value: "Every 30 Minutes"
+  },
+  {
+    id: 4,
+    value: "Hourly"
+  },
+  {
+    id: 5,
+    value: "Every 6 Hours"
+  },
+  {
+    id: 6,
+    value: "Every 12 Hours"
+  },
+  {
+    id: 7,
+    value: "Once Daily"
+  }
+];
